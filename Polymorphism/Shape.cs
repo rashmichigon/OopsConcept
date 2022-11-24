5332faa1628a51c10a9b8a510b768c138a2a9c3e
Rashmi <rashmichigon65@gmail.com>
2022-11-24 15:52:01 +0530

    Polymorphism

---
+++ Polymorphism/Shape.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept.Polymorphism
+{
+    abstract class Shape
+    {
+        public abstract int area();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,32 @@
-﻿namespace OopsConcept
+﻿
+using OopsConcept.Inheritance;
+namespace OopsConcept.Polymorphism
 {
-    using OopsConcept.Inheritance;
-    internal class Program
+    namespace OopsConcept
     {
-        static void Main(string[] args)
+
+        internal class Program
         {
-            Console.WriteLine("Hello, World!");
-            Programmer p1 = new Programmer();
-            Console.WriteLine("Salary: " + p1.salary);
-            Console.WriteLine("Bonus: " + p1.bonus);
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Choose the OOPS Concept:");
+                Console.WriteLine("1.Inheritance\n2.Polymorphism\n3.Abstraction\n4.Encapsulation");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Rectangle r = new Rectangle(10, 7);
+                        double a = r.area();
+                        Console.WriteLine("Area: {0}", a);
+                        Console.ReadLine();
+                        break;
+
+
+
+
+                }
+
+            }
         }
     }
 }
